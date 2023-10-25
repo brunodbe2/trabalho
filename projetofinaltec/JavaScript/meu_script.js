eed4cb3906c16ea4468a2d710aae5f6395383e24
brunodbe2 <117840816+brunodbe2@users.noreply.github.com>
2023-10-25 14:22:48 -0300

    Add files via upload

---
+++ projetofinaltec/JavaScript/meu_script.js
@@ -0,0 +1,9 @@
+var variavel1= prompt('fale o nome da sua mãe')
+var variavel2= prompt("com quantos anos ela deu? ")
+
+if(variavel2 <= 100){
+	document.write('sinto-lhe dizer, sua mãe é puta')
+}else{
+	document.write('puta assim mesmo')
+}
+
